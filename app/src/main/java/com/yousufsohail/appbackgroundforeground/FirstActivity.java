1a745f9aa93276ff7bdb75103199055584b7629a
Yousuf Sohail <ysohail@folio3.com>
2017-01-17 15:21:58 +0500

    Second activity added.

--- app/src/main/java/com/yousufsohail/appbackgroundforeground/FirstActivity.java
+++ app/src/main/java/com/yousufsohail/appbackgroundforeground/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.yousufsohail.appbackgroundforeground;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void gotoSecondActivity(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
     }
 }
